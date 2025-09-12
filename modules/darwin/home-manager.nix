0ea42e2feafe6acf2923aff507a99bb4f387db5f
Tetsuya <1376490336@qq.com>
2025-09-12 20:57:36 +0800

    feat: Remove alacritty and fix macos dockbar app

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -63,21 +63,7 @@ in
   local.dock = {
     enable = true;
     username = user;
-    entries = [
-      { path = "/Applications/Safari.app/"; }
-      { path = "/System/Applications/Messages.app/"; }
-      { path = "/System/Applications/Notes.app/"; }
-      { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
-      { path = "/System/Applications/Music.app/"; }
-      { path = "/System/Applications/Photos.app/"; }
-      { path = "/System/Applications/Photo Booth.app/"; }
-      { path = "/System/Applications/System Settings.app/"; }
-      {
-        path = "${config.users.users.${user}.home}/Downloads";
-        section = "others";
-        options = "--sort name --view grid --display stack";
-      }
-    ];
+    entries = [];
   };
 
 }
