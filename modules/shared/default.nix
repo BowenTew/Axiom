a329022061f5a0181502aff68d832af4a1aca100
Tetsuya <1376490336@qq.com>
2025-09-12 11:38:52 +0800

    feat: Remove Emacs fully from nix-darwin

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 let
-  emacsOverlaySha256 = "11p1c1l04zrn8dd5w8zyzlv172z05dwi9avbckav4d5fk043m754";
 in
 {
 
@@ -21,9 +20,6 @@ in
                       pathExists (path + ("/" + n + "/default.nix")))
                   (attrNames (readDir path)))
 
-      ++ [(import (builtins.fetchTarball {
-               url = "https://github.com/dustinlyons/emacs-overlay/archive/refs/heads/master.tar.gz";
-               sha256 = emacsOverlaySha256;
-           }))];
+      ++ [];
   };
 }
