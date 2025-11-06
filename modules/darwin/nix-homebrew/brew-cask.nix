598dba52ae3c641802edc12fef1c30f1f5f99fad
Tetsuya <1376490336@qq.com>
2025-11-06 13:27:28 +0800

    feat: 改一下注释

--- modules/darwin/nix-homebrew/brew-cask.nix
+++ modules/darwin/nix-homebrew/brew-cask.nix
@@ -1,29 +1,4 @@
+# List of UI applications to install via brew cask
 _:
 
-[
-  # Development Tools
-  # "homebrew/cask/docker"
-  # "visual-studio-code"
-
-  # Productivity Tools
-  # "raycast"
-
-  # Browsers
-  # "google-chrome"
-
-  # Communication Tools - Examples (uncomment as needed)
-  # "discord"
-  # "notion"
-  # "slack"
-  # "telegram"
-  # "zoom"
-
-  # Utility Tools - Examples (uncomment as needed)
-  # "syncthing"
-  # "1password"
-  # "rectangle"
-
-  # Entertainment Tools - Examples (uncomment as needed)
-  # "spotify"
-  # "vlc"
-]
+[]
