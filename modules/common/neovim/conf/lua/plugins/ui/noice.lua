abcccbf8c7a3c1a332a2548820bdb18beaa448fd
Tetsuya <1376490336@qq.com>
2026-03-16 23:40:29 +0800

    neovim remove cmdline&move cmd message to noice.lua

--- modules/common/neovim/conf/lua/plugins/ui/noice.lua
+++ modules/common/neovim/conf/lua/plugins/ui/noice.lua
@@ -1,33 +1,38 @@
 return {
-    "folke/noice.nvim",
-    cond = function()
-      return AeonVim and AeonVim.features and AeonVim.features.ui and AeonVim.features.ui.noice.enabled
-    end,
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {
-      messages = { enabled = false },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-          ["vim.lsp.util.stylize_markdown"] = false,
-          ["cmp.entry.get_documentation"] = true,
-        },
-        progress = {
-          enabled = false,
-        },
-        hover = {
-          enabled = false,
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
+  "folke/noice.nvim",
+  cond = function()
+    return AeonVim and AeonVim.features and AeonVim.features.ui and AeonVim.features.ui.noice.enabled
+  end,
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    messages = { 
+      enabled = true,  -- 改为 true
+      view = "mini",   -- 使用 mini 视图，更紧凑
     },
+    cmdline = {
+      enabled = true,
+      view = "cmdline_popup",  -- 命令行用弹窗
+    },
+    notify = {
+      enabled = true,
+      view = "notify",
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+        ["vim.lsp.util.stylize_markdown"] = false,
+        ["cmp.entry.get_documentation"] = true,
+      },
+      progress = { enabled = false },
+      hover = { enabled = false },
+    },
+    presets = {
+      bottom_search = false,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
+    },
+  },
 }
