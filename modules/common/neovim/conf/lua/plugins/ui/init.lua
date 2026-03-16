e5b174800985f0ed45c01e39336093a968399a5f
Tetsuya <1376490336@qq.com>
2026-03-16 13:03:27 +0800

    neovim support lualine plugin

--- modules/common/neovim/conf/lua/plugins/ui/init.lua
+++ modules/common/neovim/conf/lua/plugins/ui/init.lua
@@ -5,6 +5,7 @@
 return {
   { import = "plugins.ui.neo-tree" },
   { import = "plugins.ui.bufferline" },
+  { import = "plugins.ui.lualine" },
   { import = "plugins.ui.which-key" },
   { import = "plugins.ui.alpha" },
   { import = "plugins.ui.notify" },
